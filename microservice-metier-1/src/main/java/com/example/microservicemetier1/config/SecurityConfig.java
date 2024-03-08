ad094a6ebc512e2d64004ed217ac0dd9785eda4f
Rayan <rchouchane@iut.univ-paris8.fr>
2024-03-08 09:58:53 +0100

    feat: ajout generation token avec basic auth

---
+++ microservice-metier-1/src/main/java/com/example/microservicemetier1/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.example.microservicemetier1.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain apiSecurity(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/auth/user").hasRole("USER")
+                        .requestMatchers("/auth/public").permitAll()
+                        .anyRequest()
+                        .authenticated()
+                )
+                .httpBasic();
+        return http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("user"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
